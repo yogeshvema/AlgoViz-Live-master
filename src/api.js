dc4ae4a62f8ed04fc73f4fcdfba3019d396cd535
Yogesh Kumar <yogesh09155@gmail.com>
2025-12-23 00:39:07 +0530

    Connect frontend to live Render backend

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
+import axios from "axios";
 
 const api = axios.create({
-  // baseURL: 'http://127.0.0.1:8000/',
-  baseURL: "https://algoviz-api-3.onrender.com",
+  baseURL: "https://a-0tyx.onrender.com",
 });
 
 export default api;
